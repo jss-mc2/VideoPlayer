77a24b32553f746f7082d119532fd48e38296095
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-06-18 17:15:45 +0700

    Initial Commit

---
+++ VideoPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by Jason Rich Darmawan Onggo Putra on 18/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
